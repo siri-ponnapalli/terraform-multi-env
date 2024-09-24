686b910bd298f9e5e8c89e22d138c07d4726bd6d
siri-ponnapalli <ponnapallisirichandana@gmail.com>
2024-09-24 15:25:18 +0530

    workspaces

---
+++ workspaces/ec2.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "terraform" {
+    #count = terraform.workspace == "prod" ? 2 : 1
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = lookup(var.instance_type, terraform.workspace)
+    vpc_security_group_ids = ["sg-04c0a02dbdc599418"]
+    tags = {
+        Name = "terraform-${terraform.workspace}"
+    }
+}
